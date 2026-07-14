a6214748397434d686e2cf83a3e640c9fab12135
yuu-no <yusei@fout.jp>
2026-07-14 18:24:46 +0900

    feat: use hybrid OSC52 clipboard with register paste

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,5 +5,20 @@ require("config.options.ftdetect")
 
 local opt = vim.opt
 opt.timeoutlen = 100
+
+local osc52 = require("vim.ui.clipboard.osc52")
+local function paste()
+  return { vim.fn.split(vim.fn.getreg('"'), "\n"), vim.fn.getregtype('"') }
+end
+vim.g.clipboard = {
+  name = "osc52-hybrid",
+  copy = {
+    ["+"] = osc52.copy("+"),
+    ["*"] = osc52.copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+  },
+}
 opt.clipboard = "unnamedplus"
-vim.g.clipboard = "osc52"
